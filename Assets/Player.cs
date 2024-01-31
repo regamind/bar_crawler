bc7864f0d6d818ac0e3874b54572a2624c30be63
elijah hansen <elijahhansen2024@u.northwestern.edu>
2024-01-31 17:43:14 -0600

    first commit

---
+++ Assets/Player.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    public float movementSpeedVertical = 3f;
+    public float movementSpeedHorizontal = 4f;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dirX = Input.GetAxisRaw("Horizontal");
+        var dirY = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(movementSpeedHorizontal * dirX, movementSpeedVertical * dirY);
+
+
+
+        
+    }
+}

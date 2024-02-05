1f4258897477ee6da5d688882207af3f6b42b52a
Reagan Dennison <reagandennison2024@u.northwestern.edu>
2024-02-05 16:41:19 -0600

    Added health field and collision handling
    Untested right now because of no second player yet

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
     public float movementSpeedVertical = 3f;
     public float movementSpeedHorizontal = 4f;
+    public float health = 100f;
+    public bool alive = true;
 
 
 
@@ -17,8 +19,21 @@ public class Player : MonoBehaviour
         var dirY = Input.GetAxisRaw("Vertical");
         rb.velocity = new Vector2(movementSpeedHorizontal * dirX, movementSpeedVertical * dirY);
 
-
-
-        
+        // handle death here: didn't add it here since wasn't sure if we wanna implement death once we do rounds
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ((collision.collider.name == "Bottle"))
+        {
+            health -= 10f; //reducing health by 10 each time on bottle hit
+            if (health <= 0)
+            {
+                alive = false; // where death occurs, likely wanna play death animation as well
+            }
+        }
+
+    }
+
+
 }

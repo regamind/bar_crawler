297165b8be10dab08e5a45cec55bf4953c264f80
jbass23 <jordanbass2024@u.northwestern.edu>
2024-02-06 00:16:33 -0600

    added second player that can move and pick up; only player1 can throw

--- Assets/Bottle.cs
+++ Assets/Bottle.cs
@@ -8,26 +8,38 @@ public class Bottle : MonoBehaviour
     public bool onTable;
     private Rigidbody2D _rb;
     private LineRenderer _lr;
-    private Player _player;
-    private Transform _playerTransform;
-    private float _distanceToPlayer;
-    private bool _pickedUp = false;
+    private Player[] _players;
+    private Player _player1;
+    private Transform _player1Transform;
+    private float _distanceToPlayer1;
+    private Player _player2;
+    private Transform _player2Transform;
+    private float _distanceToPlayer2;
+    private bool _pickedUp1 = false;
+    private bool _pickedUp2 = false;
     private Vector3 _throwVector;
-    private float _throwPower = 5;
+    private float _throwPower = 300;
     // Start is called before the first frame update
     void Start()
     {
         onTable = true;
         _rb = GetComponent<Rigidbody2D>();
         _lr = GetComponent<LineRenderer>();
-        _player = FindObjectOfType<Player>();
+        _players = FindObjectsOfType<Player>();
+        _player1 = _players[0];
+        _player2 = _players[1];
+        Debug.Log(_player1.tag);
+        Debug.Log(_player2.tag);
     }
 
     // Update is called once per frame
     void Update()
     {
-        _playerTransform = _player.transform;
-        _distanceToPlayer = Vector3.Distance(_playerTransform.position, transform.position);
+        _player1Transform = _player1.transform;
+        _distanceToPlayer1 = Vector3.Distance(_player1Transform.position, transform.position);
+        _player2Transform = _player2.transform;
+        _distanceToPlayer2 = Vector3.Distance(_player2Transform.position, transform.position);
+
         if (Input.GetButtonDown("rBumper"))
         {
             Debug.Log("rBumper pressed");
@@ -45,18 +57,28 @@ public class Bottle : MonoBehaviour
             _rb.bodyType = RigidbodyType2D.Dynamic;
         }
 
-        if (onTable && Input.GetButtonDown("Interact") && _distanceToPlayer < 1)
+        if (onTable && Input.GetButtonDown("Interact") && _distanceToPlayer1 < 1)
         {
-            PickUp();
+            PickUp(_player1);
+            
+        }
+
+        if (onTable && Input.GetKey("m") && _distanceToPlayer2 < 1)
+        {
+            PickUp(_player2);
 
         }
 
-        if (_pickedUp)
+        if (_pickedUp1)
         {
-            transform.position = _player.transform.position + _player.transform.right*1.1f;
+            transform.position = _player1.transform.position + _player1.transform.right*1.1f;
+        }
+        else if (_pickedUp2)
+        {
+            transform.position = _player2.transform.position + _player2.transform.right * 1.1f;
         }
 
-        if (Input.GetButton("rBumper") && _pickedUp)
+        if (Input.GetButton("rBumper") && _pickedUp1)
         {
             CalculateThrowVec();
             SetTrajectory();
@@ -80,19 +102,22 @@ public class Bottle : MonoBehaviour
         }
     }
 
-    private void PickUp()
+    private void PickUp(Player player)
     {
-        transform.parent = _player.transform;
-        transform.position = _player.transform.right;
-        _pickedUp = true;
+        transform.parent = player.transform;
+        transform.position = player.transform.right;
+        if (player.tag == "Player1")
+            _pickedUp1 = true;
+        else if (player.tag == "Player2")
+            _pickedUp2 = true;
         onTable = false;
     }
 
     private void SetTrajectory()
     {
         _lr.positionCount = 2;
-        _lr.SetPosition(0, Vector3.zero);
-        _lr.SetPosition(1, _throwVector.normalized/2);
+        _lr.SetPosition(0, transform.position);
+        _lr.SetPosition(1, transform.position + _throwVector/100);
         _lr.enabled = true;
     }
 
@@ -103,8 +128,8 @@ public class Bottle : MonoBehaviour
 
     private void CalculateThrowVec()
     {
-        Vector2 joystickDir = new Vector2(Input.GetAxis("RightHorizontal"), Input.GetAxis("RightVertical"));
-        _throwVector = joystickDir.normalized/_throwPower;
+        Vector2 joystickDir = new Vector2(Input.GetAxis("RightHorizontal"), -1 * Input.GetAxis("RightVertical"));
+        _throwVector = joystickDir.normalized*_throwPower;
 
 
     }
@@ -112,6 +137,8 @@ public class Bottle : MonoBehaviour
 
     private void Throw()
     {
+        _pickedUp1 = false;
+        _pickedUp2 = false;
         _rb.AddForce(_throwVector);
     }
 
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -12,10 +12,26 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (gameObject.tag != "Dummy")
+        if (gameObject.tag == "Player1")
         {
             var dirX = Input.GetAxisRaw("Horizontal");
             var dirY = Input.GetAxisRaw("Vertical");
+            rb.velocity = new Vector2(movementSpeedHorizontal * dirX, movementSpeedVertical * dirY);
+        }
+        else if (gameObject.tag == "Player2")
+        {
+            var dirX = 0;
+            if (Input.GetKey("l"))
+                dirX += 1;
+            if (Input.GetKey("j"))
+                dirX -= 1;
+
+            var dirY = 0;
+            if (Input.GetKey("i"))
+                dirY += 1;
+            if (Input.GetKey("k"))
+                dirY -= 1;
+
             rb.velocity = new Vector2(movementSpeedHorizontal * dirX, movementSpeedVertical * dirY);
         }
     }

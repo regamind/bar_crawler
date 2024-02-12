a4537254ad494d1d33fa665dd33294d4a93dcf59
Reagan Dennison <reagandennison2024@u.northwestern.edu>
2024-02-12 02:47:51 -0600

    Respawning bottles added

---
+++ Assets/BottleSpawn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottleSpawn : MonoBehaviour
+{
+    public float spawnInterval = 10f; // Adjust as needed
+    public GameObject bottlePrefab;
+
+    private List<TableSpawnPoint> tables = new List<TableSpawnPoint>();
+
+    private void Start()
+    {
+        // Find all tables in the scene
+        TableSpawnPoint[] tableSpawnPoints = FindObjectsOfType<TableSpawnPoint>();
+        tables.AddRange(tableSpawnPoints);
+
+        // Start the spawning routine
+        StartCoroutine(SpawnBottlesRoutine());
+    }
+
+    IEnumerator SpawnBottlesRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnInterval);
+
+            // Loop through each table and check if it has a bottle
+            foreach (TableSpawnPoint table in tables)
+            {
+                if (!table.BottleOnTable)
+                {
+                    // Spawn a bottle at the table's spawn point
+                    Instantiate(bottlePrefab, table.GetSpawnPoint(), Quaternion.identity);
+                }
+            }
+        }
+    }
+}
+
--- Assets/Table.cs
+++ Assets/Table.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Table : MonoBehaviour
+public class TableSpawnPoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // public Transform spawnPoint;
+    private bool _bottleOnTable;
+
+    public bool BottleOnTable
     {
-        
+        get { return _bottleOnTable;}
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        // Check if the collider belongs to a bottle
+        if (other.CompareTag("Bottle"))
+        {
+            _bottleOnTable = true;
+            // Bottle is on the table, maybe want to adjust ranges later?
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Check if the collider belongs to a bottle
+        if (other.CompareTag("Bottle"))
+        {
+            _bottleOnTable = false;
+            // Bottle has left the table
+        }
+    }
+
+    public Vector3 GetSpawnPoint()
+    {
+        // Return the position where the bottle should spawn on this table
+        return transform.position;
     }
 }

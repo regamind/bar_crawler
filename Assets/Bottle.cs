ed6a77c9cc6536bc8b51e559b8e59ea0cec8ad99
elijahhansen <elijahhansen2024@u.northwestern.edu>
2024-02-13 14:15:20 -0600

    updated speed

--- Assets/Bottle.cs
+++ Assets/Bottle.cs
@@ -19,11 +19,12 @@ public class Bottle : MonoBehaviour
     private bool _pickedUp2 = false;
     private Vector3 _throwVector;
     private float _bottleDamage = 10f;
-    private float _throwPower = 450f;
+    private float _throwPower;
 
     // Start is called before the first frame update
     void Start()
     {
+        _throwPower = 450f;
         onTable = true;
         _rb = GetComponent<Rigidbody2D>();
         _lr = GetComponent<LineRenderer>();

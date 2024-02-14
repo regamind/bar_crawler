1acc4354664581f5cf0f7b2ce630c7530f184e98
jbass23 <jordanbass2024@u.northwestern.edu>
2024-02-13 23:21:31 -0600

    changed bottle collisions to only occur on throws and changed facing bug

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -23,10 +23,10 @@ public class Player : MonoBehaviour
 
         _spriteRenderer = GetComponent<SpriteRenderer>();
 
-        if (gameObject.tag == "Player1")
-            _spriteRenderer.flipX = false;
-        else if (gameObject.tag == "Player2")
-            _spriteRenderer.flipX = true;
+        //if (gameObject.tag == "Player1")
+        //    _spriteRenderer.flipX = false;
+        //else if (gameObject.tag == "Player2")
+        //    _spriteRenderer.flipX = true;
     }
 
 
@@ -36,53 +36,66 @@ public class Player : MonoBehaviour
     {
         var dirX = 0f;
         var dirY = 0f;
+        var rightDirX = 0f;
 
         if (gameObject.tag == "Player1")
         {
             dirX = Input.GetAxisRaw("Horizontal1");
             dirY = Input.GetAxisRaw("Vertical1");
+            rightDirX = Input.GetAxisRaw("RightHorizontal1");
         }
-        else if (gameObject.tag == "Player2")   // will need to change this to reflect second controller
+        else if (gameObject.tag == "Player2")
         {
             dirX = Input.GetAxisRaw("Horizontal2");
             dirY = Input.GetAxisRaw("Vertical2");
+            rightDirX = Input.GetAxisRaw("RightHorizontal2");
         }
 
         rb.velocity = new Vector2(movementSpeedHorizontal * dirX, movementSpeedVertical * dirY);
-        if (dirX > 0)
+        if (rightDirX > 0 || (rightDirX == 0 && dirX > 0))
             _spriteRenderer.flipX = false;
-        else if (dirX < 0)
+        else if (rightDirX < 0 || (rightDirX == 0 && dirX < 0))
             _spriteRenderer.flipX = true;
 
         // handle death here: didn't add it here since wasn't sure if we wanna implement death once we do rounds
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collider)
     {
-        if ((collision.collider.name == "Bottle"))
+        if (collider.tag == "bottle")
         {
-            Debug.Log("bottle collided in Player");
-            TakeDamage(10f);
-            //health -= 10f; //reducing health by 10 each time on bottle hit
-            if (health <= 0)
+            //if ((gameObject.tag == "Player1" && !collider.gameObject.GetComponent<Bottle>().pickedUp1) ||
+            //    (gameObject.tag == "Player2" && !collider.gameObject.GetComponent<Bottle>().pickedUp2))
+            if (!collider.gameObject.GetComponent<Bottle>().pickedUp1 && !collider.gameObject.GetComponent<Bottle>().pickedUp1)
             {
-                alive = false; // where death occurs, likely wanna play death animation as well
+                Debug.Log("bottle trigger Player");
+                TakeDamage(collider.gameObject.GetComponent<Bottle>().bottleDamage);
+                if (health <= 0)
+                {
+                    alive = false; // where death occurs, likely wanna play death animation as well
+                }
             }
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.tag == "bottle")
+        if (collision.collider.name == "Bottle")
         {
-            Debug.Log("bottle trigger Player");
-            TakeDamage(10f);
-            //health -= 10f; //reducing health by 10 each time on bottle hit
-            if (health <= 0)
-            {
-                alive = false; // where death occurs, likely wanna play death animation as well
-            }
+            
 
+            //if ((gameObject.tag == "Player1" && !collision.collider.gameObject.GetComponent<Bottle>().pickedUp1) ||
+            //    (gameObject.tag == "Player2" && !collision.collider.gameObject.GetComponent<Bottle>().pickedUp2))
+            if (!collision.collider.gameObject.GetComponent<Bottle>().pickedUp1 &&
+                    !collision.collider.gameObject.GetComponent<Bottle>().pickedUp2)
+            {
+                Debug.Log("bottle collided in Player");
+                TakeDamage(collision.collider.gameObject.GetComponent<Bottle>().bottleDamage);
+                if (health <= 0)
+                {
+                    alive = false; // where death occurs, likely wanna play death animation as well
+                }
+            }
         }
     }
 

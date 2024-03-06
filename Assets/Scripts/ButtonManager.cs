098a26afd13b4b53539d5adfc7155ce3a5ddf458
jbass23 <jordanbass2024@u.northwestern.edu>
2024-03-05 19:55:16 -0600

    made buttons make sounds

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
+    private AudioSource audioSource;
+
+    public void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void NextScene(string Scene)
     {
 
         Debug.Log("nextScene called");
+        audioSource.Play();
         SceneManager.LoadSceneAsync(Scene);
         
     }
